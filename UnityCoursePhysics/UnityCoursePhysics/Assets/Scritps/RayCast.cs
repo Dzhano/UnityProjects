2d3c1d52a38a0e48c7798b755f7b9562a2e38691
Dzhano <Djano@students.softuni.bg>
2021-05-29 20:11:19 +0300

    Untouched projects from "UNITY Basics", "Using gameobject", "Collision & physics" and "Using UI".

---
+++ UnityCoursePhysics/UnityCoursePhysics/Assets/Scritps/RayCast.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class RayCast : MonoBehaviour 
+{
+	public float hitForce = 50;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit, 20f))
+			{
+				hit.rigidbody.AddForce(Vector3.forward * hitForce, ForceMode.Impulse);
+				Debug.Log("hit : " + hit.collider.name);
+				Debug.Log("hit : " + hit.rigidbody.velocity);
+			}
+		}
+        
+	}
+}
